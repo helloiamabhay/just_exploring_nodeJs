dc7ea0703d2cc374c94fb0719cfd0d5cc8ef04af
Abhay Vishwakarma <118156018+helloiamabhay@users.noreply.github.com>
2024-12-13 14:52:03 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -30,87 +30,10 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/mern_website")
     console.log("db connected")
 
-// service.create(
-//   [
-//     {
-//       icon:"fa-sharp fa-solid fa-medal",
-//       title:"our services",
-//       discruption:"heck our services and join with us and improve your self.",
-//       linkText:"click",
-//       link:"https://www.google.com"
-//     },
-//     {
-//       icon:"fa-regular fa-dumbbell",
-//       title:"our services",
-//       discruption:"heck our services and join with us and improve your self.",
-//       linkText:"click",
-//       link:"https://www.microsoft.com"
-//     },
-//     {
-//       icon:"fa-regular fa-badminton",
-//       title:"our services",
-//       discruption:"heck our services and join with us and improve your self.",
-//       linkText:"click",
-//       link:"https://www.facebook.com"
-//     }
-
-//   ]
-// )
 
 
 
-// slider.create([
-//     {
-//    active:"empty",
-// title:"let's make fit and healthy",
-// subtitle:"fitness is the so impoetant part of life",
-// imageUrl:"/static/images/img05.jpg"
 
-// },
-// {
-//       active:"active",
-//     title:"let's make fit and healthy",
-//     subtitle:"fitness is the so impoetant part of life",
-//     imageUrl:"/static/images/img02.jpg"
-    
-//     },
-//     {
-//         active:"empty",
-//         title:"let's make fit and healthy",
-//         subtitle:"fitness is the so impoetant part of life",
-//         imageUrl:"/static/images/img03.jpg"
-        
-//         }
-// ])
-
-
-//     detail.create({
-//         brandName:"fitness website",
-//         brandIconUrl:"https://as1.ftcdn.net/v2/jpg/01/93/73/18/1000_F_193731868_afOYcVHhGIsrKB6IaO0kkQRVIXU47nvY.jpg",
-//         links:[
-//             {
-//                 label:"home",
-//                 url:"/"
-//             },
-//             {
-//                 label:"services",
-//                 url:"/services"
-//             },
-//             {
-//                 label:"about",
-//                 url:"/about"
-//             },
-//             {
-//                 label:"contact us",
-//                 url:"/contact-us"
-//             },
-//             {
-//                 label:"gallery",
-//                 url:"/gallery"
-//             }
-            
-//     ]
-//     })
 
 
   }
